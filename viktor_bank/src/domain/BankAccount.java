85a9b26c1fbc94457759285edf40e9a75994973f
Vik <lem17902@spengergasse.at>
2017-05-08 21:45:18 +0200

    BankAccount (customer_id, name, credit)

--- viktor_bank/src/domain/BankAccount.java
+++ viktor_bank/src/domain/BankAccount.java
@@ -2,5 +2,45 @@ package domain;
 
 public class BankAccount
 {
+	private int customer_id;
+	private String name;
+	private int credit;
+
+	public int getCustomer_id()
+	{
+		return customer_id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public int getCredit()
+	{
+		return credit;
+	}
+
+	public void setCustomer_id(int customer_id)
+	{
+		this.customer_id = customer_id;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public void setCredit(int credit)
+	{
+		this.credit = credit;
+	}
+
+	public BankAccount(int customer_id, String name, int credit)
+	{
+		this.customer_id = customer_id;
+		this.name = name;
+		this.credit = credit;
+	}
 
 }
